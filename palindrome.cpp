73240f6dca63600e9be8a8c403db6563ffb567be
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-06-10 16:32:31 +0200

    isPalindrome() major update

--- palindrome.cpp
+++ palindrome.cpp
@@ -2,6 +2,13 @@
 
 bool isPalindrome(const std::string & text)
 {
+    if(text.size() == 0) return false;
+    auto reverseIterator = text.rbegin();
+    for(auto forwardIterator = text.begin(); forwardIterator != text.end(); ++forwardIterator)
+    {
+        if(*forwardIterator != *reverseIterator) return false;
+        ++reverseIterator;
+    }
     return true;
 }
 
