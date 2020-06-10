edd14b0b99946b453f52e63a30bed7ef892cd6d1
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-06-10 15:34:26 +0200

    Created a dummy isPalindrome function

---
+++ palindrome.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+bool isPalindrome(const std::string & text)
+{
+    return true;
+}
+
+int main()
+{
+    std::string input{"kajak"};
+    std::cout << std::boolalpha << isPalindrome(input) << std::endl;
+    return 0;
+}
